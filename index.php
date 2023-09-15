a2ca046e0461334d55e410038c9dce34c2ed9903
BrianSanchis <brian.sanchis69@gmail.com>
2023-09-15 17:45:52 +0200

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="utf-8">
+    <title>Titre de la page</title>
+    <link rel="stylesheet" href="/css/style.css">
+    <script src="/js/script.js"></script>    
+</head>
+<body>
+    <div class="container">
+        <?php
+            echo "Hello world";
+        ?>
+    </div>
+</body>
+</html>
